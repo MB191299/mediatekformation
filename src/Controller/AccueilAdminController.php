8661fda5461f16fa1055ef17f6d2530f875859ac
Myriam Bloch <myriamelleb@gmail.com>
2024-05-24 11:04:13 +0200

    suite amlioration de la navigation en mode admin (2)

---
+++ src/Controller/AccueilAdminController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controleur de l'accueil
+ *
+ * @author emds
+ */
+class AccueilController extends AbstractController
+{
+
+    /**
+     * @var FormationRepository
+     */
+    private $repository;
+
+    /**
+     * 
+     * @param FormationRepository $repository
+     */
+    public function __construct(FormationRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route("/admin/accueil", name="accueiladmin")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        $formations = $this->repository->findAllLasted(2);
+        return $this->render("pages/accueiladmin.html.twig", [
+            'formations' => $formations
+        ]);
+    }
+
+    /**
+     * @Route("/cgu", name="cgu")
+     * @return Response
+     */
+    public function cgu(): Response
+    {
+        return $this->render("pages/cgu.html.twig");
+    }
+}
